bd4109e88430da0f9ff70510d874a0803773b499
KhurramSagoo <uaf.khurram@gmail.com>
2023-09-21 18:23:06 +0500

    LOADING ADDED

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,92 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import  { useState,useEffect } from 'react';
+import axios from 'axios';
+import Loading from './Loading';
 
-const App= ()=>{
-  const [image, setImage]=useState([]);
+const App = () => {
+  const [image, setImage] = useState([]);
+  const [isLoading, setIsLoading] = useState(false); // Initially set isLoading to false
 
-  const getImage= async ()=>{
+  const getImage = async () => {
+    setIsLoading(true); // Set isLoading to true when starting data fetching
     try {
-      const res=axios.get("https://picsum.photos/v2/list");
-      const data=(await res).data;
-      setImage(data)
-      // console.log(data);
+      const res = await axios.get("https://picsum.photos/v2/list");
+      const data = res.data;
+      setImage(data);
     } catch (error) {
-      console.log("something wrong in api")
+      console.log("something wrong in api");
+    } finally {
+      setIsLoading(false); // Set isLoading to false when data fetching is complete (success or error)
     }
-  }
+  };
 
-  return(
+  useEffect(() => {
+    getImage();
+  }, []);
 
+  return (
+    <>
+          <Loading />
 
-<>
-  
-<button onClick={getImage} style={{
-  padding:"15px 25px",
-  backgroundColor:"green",
-  display:"flex",
-  alignItems:"center",
-  justifyContent:"center",
-  width:"200px",
-  margin:"100px",
-  cursor:"pointer"
-}}>
-  click me
-</button>
+      <button
+        onClick={getImage}
+        style={{
+          padding: "15px 25px",
+          backgroundColor: "green",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "200px",
+          margin: "100px",
+          cursor: "pointer",
+          color:"white"
+        }}
+      >
+        Click me
+      </button>
 
-<br />
-{
-image.slice(11,30).map((e,id)=>{
-  return <>
-  <img src={e.download_url
-} alt=""  key={id} style={{
-  width:"369px", display:"inline-block", padding:"20px"
-}}/>
-  </>
-})
-}
-</>
+      <br />
+      {image.slice(11, 30).map((e, id) => {
+        return (
+          <img
+            src={e.download_url}
+            alt=""
+            key={id}
+            style={{
+              width: "369px",
+              display: "inline-block",
+              padding: "20px"
+            }}
+          />
+        );
+      })}
 
+      <div>
+        {isLoading ? (
+          <Loading />
+          ) : (
+          // Render your content when loading is complete
+          image.slice(11, 30).map((e, id) => {
+            return (
 
+              <img
+                src={e.download_url}
+                alt=""
+                key={id}
+                style={{
+                  width: "369px",
+                  display: "inline-block",
+                  padding: "20px"
+                }}
+              />
+            );
+          })
+          )
+        }
+        
 
+      </div>
+    </>
+  );
+};
 
-  )
-
-}
-
-export default App
+export default App;
